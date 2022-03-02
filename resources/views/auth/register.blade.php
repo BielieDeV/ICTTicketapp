c6a0558c81d8b2925a784b3c62b34d884b6de702
BaltusLX <bielie.dev.dreyer@gmail.com>
2022-03-02 21:04:36 +0200

    2022-03-02

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,14 @@
                             </div>
                         </div>
 
+                        <div class="row mb-3">
+                            <label for="cell-number" class="col-md-4 col-form-label text-md-end">{{ __('Cell Number') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="cell-number" type="text" class="form-control" name="cell-number">
+                            </div>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">

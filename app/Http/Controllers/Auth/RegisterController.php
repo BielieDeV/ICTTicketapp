57efa877908265b67012a049c5e220c0228df9e9
BaltusLX <bielie.dev.dreyer@gmail.com>
2022-03-05 07:53:53 +0200

    RegisterUser

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,6 +54,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'cell-number' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255'],
         ]);
     }
 
